bbbe961717d56ef1fb3b735a1e2b52fe4a4aade9
sasha2009 <37780107+sasha2009@users.noreply.github.com>
2019-01-03 19:15:11 +0200

    Add Login and Logout system

--- django_project/settings.py
+++ django_project/settings.py
@@ -124,4 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
